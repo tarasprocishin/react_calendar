921c67a20b5b86749d0f9a7ffa961962bca4cf7d
tarasprocishin <taras1procishin@gmail.com>
2019-07-30 22:08:50 +0300

    Updates

---
+++ precache-manifest.e94bd1fe967045b7702f20cf83fa8b97.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e29d1f0ce2a9a620255346c369a4b0cc",
+    "url": "/react_calendar/index.html"
+  },
+  {
+    "revision": "4c7d263d90d98308feaf",
+    "url": "/react_calendar/static/css/main.51d77bcf.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/react_calendar/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "4c7d263d90d98308feaf",
+    "url": "/react_calendar/static/js/main.072b544b.chunk.js"
+  },
+  {
+    "revision": "0ee6bec956579e9975c0",
+    "url": "/react_calendar/static/js/runtime~main.f8d2868a.js"
+  }
+]);
